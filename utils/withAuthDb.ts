4f1e6b8fa581780687c5012b1bc8e156e58048fe
balukrs <balukrs@outlook.com>
2025-04-21 21:09:25 +0530

    api logger added

--- utils/withAuthDb.ts
+++ utils/withAuthDb.ts
@@ -8,6 +8,15 @@ const withAuthDB =
   (handler: (req: Request) => Promise<NextResponse>) =>
   async (req: Request): Promise<NextResponse> => {
     const session = await getServerSession(authOptions);
+    const headers = req.headers;
+
+    const method = req.method;
+    const url = req.url;
+    const date = new Date().toISOString();
+    const ip = headers.get('x-forwarded-for');
+
+    // Log API requests
+    console.log(`[${date}] ${method} ${url} - IP: ${ip}`);
 
     if (!session) {
       return errorResponse({ message: 'Unauthorized' }, 401);
